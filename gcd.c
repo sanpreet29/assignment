d247f78432dfb8427c952ae2a20919c602d74040
vagrant <vagrant@myvm19>
2021-08-06 16:39:31 +0000

    recursion

---
+++ gcd.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+int GCD(int a,int b)
+{
+	if(b==0)
+	{
+		return(a);
+	}
+	else
+	{
+		if(a<b)
+		{
+			return(GCD(b,a));
+		}
+		else
+		{
+			return(GCD(b,a%b));
+		}
+	}
+}
+
+void main()
+{
+	int n1,n2,r;
+	printf("Enter any 2 numbers: ");
+	scanf("%d %d",&n1,&n2);
+	r=GCD(n1,n2);
+	printf("GCD= %d",r);
+}
